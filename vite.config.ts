5ea425aca6857f10da99aa6d39cdd8a2c9a54d66
Mccullahz <zylardmccullah@gmail.com>
2025-05-22 02:10:08 -0400

    GIANT COMMIT, added build and build scripts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+  base: '/web-v2/',
 });
 
