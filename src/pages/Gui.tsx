53d4dac2f61364a97e848010fababd078fafab46
Mccullahz <zylardmccullah@gmail.com>
2025-05-22 00:46:06 -0400

    solid initial styling and setup for the card layout, building out the rest shortly

--- src/pages/Gui.tsx
+++ src/pages/Gui.tsx
@@ -4,35 +4,28 @@ export const Gui: React.FC = () => (
   <>
     {/*need to whip up a gui page for the showcasing. ideally we can do this similar to HTML cards, and we could potentially flip the cards on hover to display more information. Front of card could display the GIF for the preview and then on hover we flip the card for displaying the info section from the terminal *info* command section*/}
     <div className="gui-container">
-      <div className="gui-card" onClick={() => window.open("/Pms", "_self")}>
-	<img
-	  src="/images/pms-prev.gif"
-	  alt="Process Management Simulator Demo"
-	  className="sim-gif"
-	/>
-	<div className="card-info">
-	  <h2>Process Management Simulator</h2>
-	  <p>
-	    A Go application that simulates how an operating system manages processes.
-	    <br />
-	  </p>
-	</div>
-	</div>
+    <div className="gui-card">
+  <div className="gui-card-inner">
+    <div className="gui-card-front">
+      <h2>Process Management Simulator</h2>
+      <img
+        src="/images/pms-prev.gif"
+        alt="Process Management Simulator Demo"
+        className="sim-gif"
+      />
+    </div>
+    <div className="gui-card-back" onClick={() => window.open("/Pms", "_self")}>
+      <strong>
+        A Go CLI + Go Wails GUI tool for simulating how an operating system manages process via scheduling algorithms
+      </strong>
+      <br />
+      <p>
+	<strong>Click for more information!</strong>
+      </p>
+    </div>
+  </div>
+</div>
 
-	<div className="gui-card" onClick={() => window.open("/Lms", "_self")}>
-	<img
-	  src="/images/lms-prev.gif"
-	  alt="LMS Demo"
-	  className="sim-gif"
-	/>
-	<div className="card-info">
-	  <h2>Library Management System</h2>
-	  <p>
-	  A Java application built to showcase OOP principles and design patterns.
-	    <br />
-	    </p>
-	    </div>
-      </div>
       </div>
   </>
 );
