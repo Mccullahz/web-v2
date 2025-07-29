928010f61ca82c659dfb7e52b7e50f1ad5edc14a
Mccullahz <zylardmccullah@gmail.com>
2025-07-29 18:10:07 -0400

    proof of concept for ticket system

--- src/components/Three.tsx
+++ src/components/Three.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Environment } from "@react-three/drei";
+import { OrbitControls, Environment, Text } from "@react-three/drei";
 //import * as THREE from "three";
 
 const Mug: React.FC<{ position: [number, number, number] }> = ({ position }) => {
@@ -23,15 +23,59 @@ const Table: React.FC = () => (
   </mesh>
 );
 
-const Ticket: React.FC<{ position: [number, number, number] }> = ({ position }) => {
+interface TicketProps {
+  position: [number, number, number];
+  title?: string;
+  category?: string;
+  tech?: string;
+  onClick?: () => void;
+}
+
+const Ticket: React.FC<TicketProps> = ({ position, title, category, tech, onClick }) => {
   const [x, , z] = position;
   const angle = Math.atan2(z, x);
-return (
-  <mesh position={position} rotation={[0, angle, 0]}>
-    <boxGeometry args={[0.6, 0.1, 0.5]} />
-    <meshStandardMaterial color="white" />
-  </mesh>
-)
+
+  return (
+    <group position={position} rotation={[0, angle, 0]} onClick={onClick}>
+      {/* ticket mesh */}
+      <mesh>
+        <boxGeometry args={[0.6, 0.1, 0.5]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
+
+      {/* text for ticket description, title desc, tech etc I hate doing it this way but i do not know of a more resuable manner, will adjust colors to better fit theme once poc is done */}
+      <Text
+        position={[0, 0.06, 0.1]}
+        fontSize={0.05}
+        color="black"
+        maxWidth={0.5}
+        anchorX="center"
+        anchorY="middle"
+      >
+        {title}
+      </Text>
+      <Text
+        position={[0, 0.06, 0]}
+        fontSize={0.04}
+        color="gray"
+        maxWidth={0.5}
+        anchorX="center"
+        anchorY="middle"
+      >
+        {category}
+      </Text>
+      <Text
+        position={[0, 0.06, -0.1]}
+        fontSize={0.035}
+        color="darkslategray"
+        maxWidth={0.5}
+        anchorX="center"
+        anchorY="middle"
+      >
+        {tech}
+      </Text>
+    </group>
+  );
 };
 
 const Lights = () => (
@@ -54,7 +98,12 @@ export const ThreeScene: React.FC = () => {
       <Table />
       {/*everything on the table here*/}
       <Mug position={[1, 0.3, 0]} />
-      <Ticket position={[1.6, 0.01, 0]} />
+      <Ticket position={[1.6, 0.01, 0]} 
+	title="Proof of Concept"
+	category="Frontend Web Development"
+	tech="React, TypeScript"
+	onClick={() => console.log("Ticket clicked!")}
+      />
 
       <Mug position={[-1, 0.3, 0]} />
       <Ticket position={[-1.6, 0.01, 0]} />
